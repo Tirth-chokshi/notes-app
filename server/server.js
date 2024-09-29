d99b13d83933df618cc96461201506c3dd4094a5
Tirth chokshi <chokshitirth4@gmail.com>
2024-09-29 21:30:54 +0530

    url changed

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000"],
+    origin: ["https://notes-app-production-e3a2.up.railway.app"],
   })
 );
 
