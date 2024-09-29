052fd11ac525284a5136213e6db462ef3343558e
Tirth chokshi <chokshitirth4@gmail.com>
2024-09-29 21:47:13 +0530

    api updated

--- front/app/lib/api.js
+++ front/app/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://notes-app-production-e3a2.up.railway.app',
   withCredentials: true,
 });
 
